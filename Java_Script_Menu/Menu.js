aec6793ed858bb817be83fa1f419d11a06850037
unknown <lazada@LEL-ID-L-W-1287.lazada.intlaz>
2022-11-16 12:46:40 +0700

    tambah layer

--- Java_Script_Menu/Menu.js
+++ Java_Script_Menu/Menu.js
@@ -1,2 +1,3 @@
-const posisi = document.getElementById('center');
-posisi.alert('aku sayang kamu');
+const copy = document.getElementById('copyright');
+copy.InnerHTML ="aku sayang kmau";
+copy.style.Color = "white";
