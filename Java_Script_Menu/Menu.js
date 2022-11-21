1a2704dbd007ab6ce6b0e7f8277c025714fe0ce6
unknown <lazada@LEL-ID-L-W-1287.lazada.intlaz>
2022-11-21 13:17:19 +0700

    edit gambar

--- Java_Script_Menu/Menu.js
+++ Java_Script_Menu/Menu.js
@@ -1,3 +1,6 @@
 const copy = document.getElementById('copyright');
 copy.InnerHTML ="aku sayang kmau";
 copy.style.Color = "white";
+
+const bodi  = Document.querysellectorAll('body');
+console.log('selamat datang di BLC');
