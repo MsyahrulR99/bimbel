a906eea1a4d47d450dc5ad2856156deed35af4ef
unknown <lazada@LEL-ID-L-W-1287.lazada.intlaz>
2022-11-26 11:05:51 +0700

    edit

--- Java_Script_Login/Login.js
+++ Java_Script_Login/Login.js
@@ -1,5 +1,14 @@
 const nama = document.getElementById("mail");
-nama.innerHTML = "Email:";
+innerHTML = "Email:";
 
 const warna = document.getElementById("nama");
 warna.style.color = "#23323";
+
+
+const bodi = document.getElementById('bodi');
+bodi.body.style.background = 'url("Image/guru.jpg")';
+bodi.body.style.backgroundSize= '1600px';
+
+const berwarna = document.getElementById("klik");
+berwarna function(o);{
+berwarna.style.backgroundColor = "red";
