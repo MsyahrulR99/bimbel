7b2c0abd3e70b3f4bc307dbc0737762aa47e6936
unknown <lazada@LEL-ID-L-W-1287.lazada.intlaz>
2022-11-29 19:29:53 +0700

    menambah file dan meng aktifkan sosmed

---
+++ Java_Script_FAQ/FAQ.js
@@ -0,0 +1,14 @@
+const color = document.getElementById('color');
+color.style.backgroundColor = "orange";
+
+const kelas = document.getElementById('kelas');
+kelas.style.backgroundColor = "orange";
+
+const daftar = document.getElementById('pendaftaran');
+daftar.style.backgroundColor = "orange";
+
+
+
+const about= document.getElementById('about');
+about.style.backgroundColor = "black";
+about.style.color = "white";
